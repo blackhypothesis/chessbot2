e928b553d51564e785ff1dfa756342aac17caabe
Marcel <sleep24@bluewin.ch>
2024-09-11 19:01:36 +0200

    starting with datastructure to hold all information necessary; interface for chessbot

--- moves.go
+++ moves.go
@@ -184,28 +184,6 @@ func playMoveWithMouse(driver selenium.WebDriver, is_white_orientation bool) (fu
 	}, nil
 }
 
-func getCoordinate(x string) int {
-	switch x {
-	case "a":
-		return 0
-	case "b":
-		return 1
-	case "c":
-		return 2
-	case "d":
-		return 3
-	case "e":
-		return 4
-	case "f":
-		return 5
-	case "g":
-		return 6
-	case "h":
-		return 7
-	}
-	return 0
-}
-
 func getTimeLeftSeconds(driver selenium.WebDriver) ([2]int, error) {
 	time_left, err := driver.FindElements(selenium.ByClassName, "time")
 	if err != nil {
